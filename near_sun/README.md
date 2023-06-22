8e64c21dc3320318a8df88ef94ec1e90cab1b72d
Peter Yoachim <yoachim@uw.edu>
2023-06-21 17:09:23 -0700

    near_sun

---
+++ near_sun/README.md
@@ -0,0 +1,1 @@
+Currently the near sun survey doesn't seem to have any regulation on the filter distribution. Let's add a basis function to try and even it out a bit.
---
+++ near_sun/near_sun.sh
@@ -0,0 +1,7 @@
+python near_sun.py --filter_dist_weight 0 
+python near_sun.py --filter_dist_weight 0.01 
+python near_sun.py --filter_dist_weight 0.03
+python near_sun.py --filter_dist_weight 0.3
+python near_sun.py --filter_dist_weight 1
+python near_sun.py --filter_dist_weight 3 
+python near_sun.py --filter_dist_weight 10 
