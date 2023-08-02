f9eb1bbaf1e22083072a8712a1c8aced6ef94265
Peter Yoachim <yoachim@uw.edu>
2023-08-02 14:11:44 -0700

    more attempts

--- technical/filter_twi/README.md
+++ technical/filter_twi/README.md
@@ -1,1 +1,4 @@
 See if we can eliminate the rapid filter swaps that happen once a month in twilight
+
+filter_stay.py:  turn up the weight on staying in the same filter--down to 204 fast filter swaps, so better but not much
+filter_simple.py: Use a more simple basis function for weighting staying in the same filter
---
+++ technical/filter_twi/filters.sh
@@ -0,0 +1,8 @@
+python filter_stay.py
+python filter_simple.py --filter_weight 0
+python filter_simple.py --filter_weight 0.1
+python filter_simple.py --filter_weight 1
+python filter_simple.py --filter_weight 3
+python filter_simple.py --filter_weight 10
+python filter_simple.py --filter_weight 30
+python filter_simple.py --filter_weight 100
