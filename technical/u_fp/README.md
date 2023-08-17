10b09da4b6055cf60eb652305f6dadd338c36322
Peter Yoachim <yoachim@uw.edu>
2023-08-16 22:22:12 -0700

    u_fp

---
+++ technical/u_fp/README.md
@@ -0,0 +1,1 @@
+Try cranking up the u footprint to see if that fixes the strange pause when no u band observations are taken
---
+++ technical/u_fp/u_fp.sh
@@ -0,0 +1,7 @@
+python u_fp.py --u_override 0.32
+python u_fp.py --u_override 0.16
+python u_fp.py --u_override 0.38
+python u_fp.py --u_override 0.40
+python u_fp.py --u_override 0.45
+python u_fp.py --u_override 0.5
+python u_fp.py --u_override 1.0
